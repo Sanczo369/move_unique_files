32afe46ddeed7007853e9f89130523ab0cfa9334
Arek Sanecki <areksanecki@gmail.com>
2023-05-28 22:00:47 +0200

    add file_list()

--- lista_plikow/main.py
+++ lista_plikow/main.py
@@ -1,8 +1,13 @@
+import os
 from tkinter import *
 root = Tk()
 root.title("Lista Plików")
-f = open("wynik.txt",mode='w')
-f.write("test")
-f.close()
+def file_list():
+    f = open("wynik.txt",mode='w')
+    folder_zrodlowy=input("Podaj sciezke do folderu")
+    for nazwa_pliku in os.listdir(folder_zrodlowy):
+        f.write(nazwa_pliku+"\n")
+    f.close()
 if __name__ == '__main__':
-    root.mainloop()
+    #root.mainloop()
+    file_list()
