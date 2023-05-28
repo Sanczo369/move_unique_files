4e76e6b16fd151606a743af8625f35c9d4ee3519
Arek Sanecki <areksanecki@gmail.com>
2023-05-28 22:26:41 +0200

    add UI

--- lista_plikow/main.py
+++ lista_plikow/main.py
@@ -8,6 +8,20 @@ def file_list():
     for nazwa_pliku in os.listdir(folder_zrodlowy):
         f.write(nazwa_pliku+"\n")
     f.close()
+
+# Elements
+folder_label = Label(root, text="Podaj ścieżkę do folderu")
+name_label = Label(root, text="Podaj nazwe dla nowo powstalego pliku")
+folder_entry = Entry(root)
+name_entry = Entry(root)
+generate_btn = Button(root, text="Generuj")
+# Position
+folder_label.grid(row=0, column=0)
+folder_entry.grid(row=0, column=1)
+name_label.grid(row=1, column=0)
+name_entry.grid(row=1, column=1)
+generate_btn.grid(row=2, columnspan=2)
+
 if __name__ == '__main__':
-    #root.mainloop()
-    file_list()
+    root.mainloop()
+    #file_list()
