a2d1a45f68b93f5760d4d3fd01b10ab58f827004
Arek Sanecki <areksanecki@gmail.com>
2023-05-29 03:27:21 +0200

    add function to btn

--- lista_plikow/main.py
+++ lista_plikow/main.py
@@ -2,19 +2,24 @@ import os
 from tkinter import *
 root = Tk()
 root.title("Lista Plików")
-def file_list():
-    f = open("wynik.txt",mode='w')
-    folder_zrodlowy=input("Podaj sciezke do folderu")
+def file_list(folder,name):
+    if len(folder)==0 or len(name)==0:
+        name="lista"
+        folder="C:\\"
+    file_name=name+".txt"
+    f = open(file_name,mode='w')
+    folder_zrodlowy=folder
     for nazwa_pliku in os.listdir(folder_zrodlowy):
         f.write(nazwa_pliku+"\n")
     f.close()
+    root.quit()
 
 # Elements
 folder_label = Label(root, text="Podaj ścieżkę do folderu")
 name_label = Label(root, text="Podaj nazwe dla nowo powstalego pliku")
 folder_entry = Entry(root)
 name_entry = Entry(root)
-generate_btn = Button(root, text="Generuj")
+generate_btn = Button(root, text="Generuj", command=lambda: file_list(folder_entry.get(),name_entry.get()))
 # Position
 folder_label.grid(row=0, column=0)
 folder_entry.grid(row=0, column=1)
